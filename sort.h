dee0c59c30085b8a41e9323e4caa773f59735ae9
amine <mohamedaminebk99@gmailcom>
2024-02-19 05:07:33 -0800

    advanced

--- sort.h
+++ sort.h
@@ -1,8 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _SORT_H_
+#define _SORT_H_
 #include <stddef.h>
 
 /**
@@ -18,22 +15,22 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-/*printing tools*/
+
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
-/*quick sort functions*/
 void quick_sort(int *array, size_t size);
-void quicksort(int *array, int low, int heigh, size_t size);
-int partition(int *array, int low, int high, size_t size);
-void swap(int *array, size_t a, size_t b);
 void shell_sort(int *array, size_t size);
-/*Cocktail shaker sort functions*/
+void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void swap_nodes(listint_t *a, listint_t *b);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 
 #endif
